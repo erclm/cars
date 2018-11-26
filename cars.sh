1d412903b3a91988cc501d47739c0bb018625bef
Eric Lim <jilim@chapman.edu>
2018-11-26 10:38:24 -0800

    fin

---
+++ cars.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# cars.sh
+# Eric Lim
+int i = 0;
+while [[ i==0 ]]; do
+  echo "My Old Cars Inventory System";
+  echo "----------------------------";
+  echo "1. Enter a Car";
+  echo "2. List the cars";
+  echo "Q. Quit";
+  read option;
+  case "$option" in
+    "1" ) echo "Year?";
+          read a;
+          echo "Make?";
+          read b;
+          echo "Model?";
+          read c;
+          d="$a:$b:$c";
+          echo "$d" >> my_old_cars;
+      ;;
+    "2" ) cat my_old_cars;
+      ;;
+    "Q" ) break;
+      ;;
+    * ) echo "Illegal Input"
+      ;;
+  esac
+done
